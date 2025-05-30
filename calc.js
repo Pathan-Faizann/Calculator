3b82eb14b709e49abce01e664b5d0a2f5a972375
Pathan Faizan <faizanpathan8104@gmail.com>
2025-05-30 14:27:17 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,53 @@
+
+let display = document.querySelector(".display");
+let currentInput = "";
+let previousInput = null;
+let operator = null;
+
+
+let buttons = document.querySelectorAll('.buttons button');
+buttons.forEach((button) => {
+  button.addEventListener('click',()=>{
+    const value = button.value;
+    console.log(value.charCodeAt())
+    if(!isNaN(value)){
+      currentInput+= value;
+      display.innerText = currentInput;
+    }
+    else if(value === "+" || value ==="-" || value ==="x"|| value ==="/"){
+      operator = value;
+      previousInput = parseFloat(currentInput);
+      currentInput = '';
+      // let result = currentInput + previousInput;
+    }
+    else if(value === "="){
+      if(operator && previousInput !== null && currentInput!== ""){
+          let result;
+      if(operator==="+"){
+        result = previousInput + parseFloat(currentInput);
+        console.log("success")
+      }
+      else if(operator === "-"){
+        result = previousInput - parseFloat(currentInput);
+      }
+      else if(operator === "x"){
+        result = previousInput * parseFloat(currentInput)
+      }
+      else if(operator === "/"){
+        console.log("success");
+        result = previousInput / parseFloat(currentInput);
+        
+      }
+      console.log(operator)
+      display.innerText = result;
+      currentInput = result.toString();
+      previousInput = null;
+      operator = null;
+      }
+    
+    }
+  })
+  
+});
+
+
